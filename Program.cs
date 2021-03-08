c5207dde7acce7ecf70d1fe7dfa9cf53ab2c58c5
rashed99 <71206336+rashed99@users.noreply.github.com>
2021-03-08 21:13:52 +0600

    Add files via upload
    Print n to 1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_1_to_n
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Number :");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
